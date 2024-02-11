7b8904a3296197a7a48e312d3959f661a2bba781
Slibul <wlalsaka000@gmail.com>
2024-02-11 20:08:26 +0900

    Initial commit

---
+++ DeadlyPath.cs
@@ -0,0 +1,195 @@
+using System;
+using System.Collections.Generic;
+using Characters;
+using Characters.Abilities.Darks;
+using Services;
+using Singletons;
+using HarmonyLib;
+using Level;
+using Hardmode;
+using UnityEngine;
+using Data;
+using Runnables;
+using Level.Waves;
+
+namespace DeadlyOfWorld;
+
+public class DeadlyPath
+{
+    [HarmonyPostfix]
+    [HarmonyPatch((typeof(Character)), nameof(Character.InitializeActions))]
+    static void CharacterPostfix(ref Character __instance)
+    {
+        List<Character> characters = new List<Character>();
+        characters.Add(__instance);
+        DarkEnemySelector.instance.ElectIn(characters);
+    }
+
+
+    [HarmonyPostfix]
+    [HarmonyPatch((typeof(DarkEnemySelector)), nameof(DarkEnemySelector.ElectIn))]
+    static void SetCounters(object[] __args, ref DarkAbilityConstructor[] ____constructors)
+    {
+        if (Plugin.isNoDarker.Value)
+        {
+            return;
+        }
+
+        if (Plugin.isParamet.Value == true)
+        {
+            System.Random random = new System.Random();
+            int num = random.Next(1, 101);
+            if (num >= Plugin.isSetP.Value)
+            {
+                foreach (ICollection<Character> candidates in __args)
+                {
+                    foreach (Character c in candidates)
+                    {
+                        if (c.type == Character.Type.TrashMob && c.key != Key.Hound && c.key != Key.SpiritInFlask && c.key != Key.UnstableFlasksSpirit)
+                            ____constructors[Singleton<HardmodeManager>.Instance.currentLevel].Provide(c); // 모든적이 나와요
+                    }
+                }
+            }
+            return;
+        }
+
+        if (Plugin.Step.Value == 1)
+        {
+            foreach (ICollection<Character> candidates in __args)
+            {
+                foreach (Character c in candidates)
+                {
+                    if (c.type == Character.Type.TrashMob && c.key != Key.Hound && c.key != Key.SpiritInFlask && c.key != Key.UnstableFlasksSpirit)
+                        ____constructors[Singleton<HardmodeManager>.Instance.currentLevel].Provide(c); // 모든적이 나와요
+                }
+            }
+        }
+        else if (Plugin.Step.Value == 11)
+        {
+            RarityPossibilities _rarityPossibilities = new RarityPossibilities();
+            Rarity _rarity;
+            Chapter currentChapter = Singleton<Service>.Instance.levelManager.currentChapter;
+
+            System.Random random = new System.Random(
+            GameData.Save.instance.randomSeed
+            + 2028506624
+            + (int)currentChapter.type * 256
+            + currentChapter.stageIndex * 16
+            + currentChapter.currentStage.pathIndex
+            );
+            int num = random.Next(0, 100) + 1;
+            _rarity = _rarityPossibilities.Evaluate(random);
+            if (_rarity == Rarity.Common)
+            {
+                foreach (ICollection<Character> candidates in __args)
+                {
+                    Debug.LogError(candidates.Count);
+                    foreach (Character c in candidates)
+                    {
+                        if (c.type == Character.Type.TrashMob && c.key != Key.Hound && c.key != Key.SpiritInFlask && c.key != Key.UnstableFlasksSpirit)
+                            ____constructors[Singleton<HardmodeManager>.Instance.currentLevel].Provide(c);
+                    }
+                }
+            }
+        }
+        else if (Plugin.Step.Value == 12)
+        {
+            RarityPossibilities _rarityPossibilities = new RarityPossibilities();
+            Rarity _rarity;
+            Chapter currentChapter = Singleton<Service>.Instance.levelManager.currentChapter;
+
+            System.Random random = new System.Random(
+            GameData.Save.instance.randomSeed
+            + 2028506624
+            + (int)currentChapter.type * 256
+            + currentChapter.stageIndex * 16
+            + currentChapter.currentStage.pathIndex
+            );
+            int num = random.Next(0, 100) + 1;
+            _rarity = _rarityPossibilities.Evaluate(random);
+            if (_rarity == Rarity.Rare)
+            {
+                foreach (ICollection<Character> candidates in __args)
+                {
+                    Debug.LogError(candidates.Count);
+                    foreach (Character c in candidates)
+                    {
+                        if (c.type == Character.Type.TrashMob && c.key != Key.Hound && c.key != Key.SpiritInFlask && c.key != Key.UnstableFlasksSpirit)
+                            ____constructors[Singleton<HardmodeManager>.Instance.currentLevel].Provide(c);
+                    }
+                }
+            }
+        }
+        else if (Plugin.Step.Value == 13)
+        {
+            RarityPossibilities _rarityPossibilities = new RarityPossibilities();
+            Rarity _rarity;
+            Chapter currentChapter = Singleton<Service>.Instance.levelManager.currentChapter;
+
+            System.Random random = new System.Random(
+            GameData.Save.instance.randomSeed
+            + 2028506624
+            + (int)currentChapter.type * 256
+            + currentChapter.stageIndex * 16
+            + currentChapter.currentStage.pathIndex
+            );
+            int num = random.Next(0, 100) + 1;
+            _rarity = _rarityPossibilities.Evaluate(random);
+            if (_rarity == Rarity.Unique)
+            {
+                foreach (ICollection<Character> candidates in __args)
+                {
+                    Debug.LogError(candidates.Count);
+                    foreach (Character c in candidates)
+                    {
+                        if (c.type == Character.Type.TrashMob && c.key != Key.Hound && c.key != Key.SpiritInFlask && c.key != Key.UnstableFlasksSpirit)
+                            ____constructors[Singleton<HardmodeManager>.Instance.currentLevel].Provide(c);
+                    }
+                }
+            }
+        }
+        else if (Plugin.Step.Value == 14)
+        {
+            RarityPossibilities _rarityPossibilities = new RarityPossibilities();
+            Rarity _rarity;
+            Chapter currentChapter = Singleton<Service>.Instance.levelManager.currentChapter;
+
+            System.Random random = new System.Random(
+            GameData.Save.instance.randomSeed
+            + 2028506624
+            + (int)currentChapter.type * 256
+            + currentChapter.stageIndex * 16
+            + currentChapter.currentStage.pathIndex
+            );
+            int num = random.Next(0, 100) + 1;
+            _rarity = _rarityPossibilities.Evaluate(random);
+            if (_rarity == Rarity.Legendary)
+            {
+                foreach (ICollection<Character> candidates in __args)
+                {
+                    Debug.LogError(candidates.Count);
+                    foreach (Character c in candidates)
+                    {
+                        if (c.type == Character.Type.TrashMob && c.key != Key.Hound && c.key != Key.SpiritInFlask && c.key != Key.UnstableFlasksSpirit)
+                            ____constructors[Singleton<HardmodeManager>.Instance.currentLevel].Provide(c);
+                    }
+                }
+            }
+        }
+        else
+        {
+            Plugin.Step.Value = 1;
+            foreach (ICollection<Character> candidates in __args)
+            {
+                Debug.LogError(candidates.Count);
+                foreach (Character c in candidates)
+                {
+                    if (c.type == Character.Type.TrashMob && c.key != Key.Hound && c.key != Key.SpiritInFlask && c.key != Key.UnstableFlasksSpirit)
+                        ____constructors[Singleton<HardmodeManager>.Instance.currentLevel].Provide(c);
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using DeadlyOfWorld;
+using HarmonyLib;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    public static ConfigEntry<int> Step;
+    public static ConfigEntry<int> isSetP;
+    public static ConfigEntry<bool> isNoDarker;
+    public static ConfigEntry<bool> isParamet;
+    private void Awake()
+    {
+        Step = Config.Bind<int>("General", "난이도", 1, "난이도 : 1\t1.전원검은적\n난이도 : 11\t11.일반적으로 많이나옵니다.\n난이도 : 12\t12.레어하게 많이나옵니다.\n난이도 : 13\t13.유니크하게 나옵니다.\n난이도 : 14\t14.레전드확율로 나옵니다.");
+        Step = Config.Bind<int>("General", "확율", 0, "0~100까지의확율로 검은적이 나오게 설정할수있습니다.");
+        isNoDarker = Config.Bind<bool>("General", "검은적 정상화", false, "검은적이 나오는것을 되돌립니다.");
+        isParamet = Config.Bind<bool>("General", "검은적 확율화", false, "검은적이 나오는것을 직접설정하게 바꿉니다.");
+        Harmony.CreateAndPatchAll(typeof(DeadlyPath));
+        Logger.LogInfo($"Mod {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    private void Update()
+    {
+        Config.Reload();
+    }
+}
